e2884e645773bdf05a82449a05107616a92db418
Sachin Bhoi <givesachin@gmail.com>
2025-08-25 13:26:22 +0530

    WIP: teams and players APIs.

---
+++ laravel-teams-players/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\PlayerController;
+
+Route::apiResource('teams', TeamController::class);
+Route::apiResource('players', PlayerController::class);
